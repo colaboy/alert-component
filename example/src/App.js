754d1529781c9efd7cf7aa91316fe21e713bb9de
wangmingzhu <wangmingzhu@waiqin365.com>
2018-08-25 15:33:23 +0800

    init

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'alert-component'
+import {ExampleComponent} from 'alert-component'
 
 export default class App extends Component {
   render () {
